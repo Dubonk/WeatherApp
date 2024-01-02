34dd9375fffc3226a459748df8b1a9d4350065e3
Kevin <kevindubon43@gmail.com>
2024-01-02 12:34:52 -0800

    Cleared template info

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Koni's Place",
+            title: "Weather App",
         }),
     ],
     module: {
