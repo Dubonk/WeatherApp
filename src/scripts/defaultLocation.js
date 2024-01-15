a282c2e7aaaa24e118703f87dacfdc953d000311
Kevin <kevindubon43@gmail.com>
2024-01-15 11:25:53 -0800

    Fixed code to adjust for free version of weather api

--- src/scripts/defaultLocation.js
+++ src/scripts/defaultLocation.js
@@ -28,31 +28,33 @@ const getLaWeather = async function () {
     const day3Icon = document.getElementById('day3Icon');
     const day3Temp = document.getElementById('day3Temp');
     try{
-        const forecastResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=feb7b7bfa5f543b49ee173908232912&q=${userInput}&days=5`);
+        const forecastResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=feb7b7bfa5f543b49ee173908232912&q=${userInput}&days=3`);
         const forecastData = await forecastResponse.json();
-        const dateString1 = forecastData.forecast.forecastday[2].date;
-        const dateString2 = forecastData.forecast.forecastday[3].date;
-        const dateString3 = forecastData.forecast.forecastday[4].date;
-        const date1 = new Date(dateString1);
-        const date2 = new Date(dateString2);
-        const date3 = new Date(dateString3);
-        const options = { weekday: 'long' };
+        const dateString1 = forecastData.forecast.forecastday[0].date;
+        const dateString2 = forecastData.forecast.forecastday[1].date;
+        const dateString3 = forecastData.forecast.forecastday[2].date;
+
+        const date1 = new Date(dateString1 + ' PST');
+        const date2 = new Date(dateString2 + ' PST');
+        const date3 = new Date(dateString3 + ' PST');
+        const options = { weekday: 'long'};
         const dayName1 = new Intl.DateTimeFormat('en-US', options).format(date1); 
         const dayName2 = new Intl.DateTimeFormat('en-US', options).format(date2);
         const dayName3 = new Intl.DateTimeFormat('en-US', options).format(date3);
 
         currentLow.textContent = `Low: ${forecastData.forecast.forecastday[0].day.mintemp_f}°F`;
-        day1Date.textContent = dayName1;
-        day1Icon.src = `https:${forecastData.forecast.forecastday[1].day.condition.icon}`;
-        day1Temp.innerHTML = `High: ${forecastData.forecast.forecastday[1].day.avgtemp_f}°F <br>Low: ${forecastData.forecast.forecastday[1].day.mintemp_f}°F`;
+        day1Date.textContent = dayName1.substring(0, 3);
+        day1Icon.src = `https:${forecastData.forecast.forecastday[0].day.condition.icon}`;
+        day1Temp.innerHTML = `High: ${forecastData.forecast.forecastday[0].day.avgtemp_f}°F <br>Low: ${forecastData.forecast.forecastday[0].day.mintemp_f}°F`;
 
-        day2Date.textContent = dayName2;
-        day2Icon.src = `https:${forecastData.forecast.forecastday[2].day.condition.icon}`;
-        day2Temp.innerHTML = `High: ${forecastData.forecast.forecastday[2].day.avgtemp_f}°F <br>Low: ${forecastData.forecast.forecastday[2].day.mintemp_f}°F`;
+        day2Date.textContent = dayName2.substring(0, 3);
+        day2Icon.src = `https:${forecastData.forecast.forecastday[1].day.condition.icon}`;
+        day2Temp.innerHTML = `High: ${forecastData.forecast.forecastday[1].day.avgtemp_f}°F <br>Low: ${forecastData.forecast.forecastday[1].day.mintemp_f}°F`;
 
-        day3Date.textContent = dayName3;
-        day3Icon.src = `https:${forecastData.forecast.forecastday[3].day.condition.icon}`;
-        day3Temp.innerHTML = `High: ${forecastData.forecast.forecastday[3].day.avgtemp_f}°F <br>Low: ${forecastData.forecast.forecastday[3].day.mintemp_f}°F`;
+        day3Date.textContent = dayName3.substring(0, 3);
+        day3Icon.src = `https:${forecastData.forecast.forecastday[2].day.condition.icon}`;
+        day3Temp.innerHTML = `High: ${forecastData.forecast.forecastday[2].day.avgtemp_f}°F <br>Low: ${forecastData.forecast.forecastday[2].day.mintemp_f}°F`;
+        
     } catch (error) {
         console.log(error);
     }
